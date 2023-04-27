8b833a98051fe69f195c7667323e25f1f2f92c61
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2023-04-27 13:05:46 -0300

    maquetado sección portfolio y contacto

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*-----Functiones Anónimas Autoejecutables-----self-executing anonymous functions-------- */
+
 /*---------------Menu--------------- */
 
 ((d) => {
@@ -16,5 +18,41 @@
     $btnMenu.firstElementChild.classList.remove("none");
     $btnMenu.lastElementChild.classList.add("none");
     $menu.classList.remove("is-active");
+  });
+})(document);
+
+/* ********** ContactForm ********** */
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/mauriciotauro82@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        location.hash = "#thank-you";
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "An error occurred while sending, please try again";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#close";
+        }, 3000);
+      });
   });
 })(document);
